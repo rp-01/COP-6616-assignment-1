9a23e972f7a96e2d32fb471f796abbc83c6b811d
Raj Parekh <rajparekh001@outlook.com>
2020-09-09 22:44:17 -0400

    update readme and source code

--- findPrimes.cpp
+++ findPrimes.cpp
@@ -4,7 +4,7 @@
 #include <algorithm> //sort vector to print top 10 primes
 #include <thread>
 #include <atomic> // atomic counter
-#include <ctime> // calculate exec time
+#include <ctime> // calculate CPU time
 #include <mutex> // lock vector when storing primes
 #include <math.h> // sqrt when finding prime number
 
@@ -20,7 +20,9 @@ int main()
     std::atomic_uint counter{0}; // atomic variable 
     std::vector<std::thread> threads; 
     
-	unsigned int startTime = clock(); //capture start time
+	unsigned int cpuStartTime = clock(); //capture cpu start time
+	auto wallStartTime = std::chrono::high_resolution_clock::now(); //capture wall clock start time
+ 	
 	
     for (unsigned int i = 0; i < 8; ++i) {
         threads.emplace_back(
@@ -28,27 +30,28 @@ int main()
                 bool flag; // to determine prime number
                 for (unsigned int num; (num = counter.fetch_add(1)) < MAX; ) { 
                     flag = true;
-			// skip 0 and 1
-			if(num == 0 || num == 1 || ((num % 2 == 0) && (num > 2))){
-				continue;	
-			}
+				// skip 0 and 1				
+				if(num == 0 || num == 1 || ((num % 2 == 0) && (num > 2))){
+					continue;	
+				}
 				
 				
                     for (unsigned int k = 2; k <= sqrt(num); ++k) {
-			    if(num == 2){
-				    break;					
-			    }
-			    if (num % k == 0) {
-                            	flag = false;
-                            	break;
-			    }
+						if(num == 2){
+							break;					
+						}
+                        if (num % k == 0) {
+                            flag = false;
+                            break;
+                        }
                     }
                     isPrime[num] = flag;
-			if(flag == true){
-				vectLock.lock();
+                    
+					if(flag == true){
+						 vectLock.lock();
                 		listOfPrimes.push_back(num);
                 		vectLock.unlock();
-			}
+					}
                 }
             }
         );
@@ -57,20 +60,23 @@ int main()
     for (auto & t : threads) {
         t.join();
     }
-	
-	unsigned int endTime = clock(); //capture end time
+	unsigned int cpuEndTime = clock(); //capture cpu end time
+	auto wallEndTime = std::chrono::high_resolution_clock::now(); //capture wall clock end time
 
-	//print execution time in seconds
-	std::cout << "Execution Time : " << 
-            (endTime - startTime)/double(CLOCKS_PER_SEC) 
-            << " seconds" << "\n" << std::endl;
+	//print CPU time
+	std::cout << "CPU Time: " << 
+            (cpuEndTime - cpuStartTime)/double(CLOCKS_PER_SEC) << "\n" << std::endl;
+
+	// print wall clock time
+	auto wallClock = std::chrono::duration_cast<std::chrono::seconds>( wallEndTime - wallStartTime ).count();
+	std::cout << "Wall clock Time: "<< wallClock << "\n" << std::endl;
 
 	//print total number of primes
 	unsigned int totalPrimes = 0;
 	for (int i = 0; i < isPrime.size(); i++){
 		if(isPrime[i] == true){
 			totalPrimes += 1;		
-		}	
+		}
 	}
     std::cout << "number of primes found: " << totalPrimes << "\n" << std::endl;
 	
@@ -90,3 +96,5 @@ int main()
 
     return 0;
 }
+
+
